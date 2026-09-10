988a972084d34234c30e826d5ebb554e7743071c
zettoesports-glitch <zettoesports@gmail.com>
2026-09-10 15:01:18 -0700

    phase2: add persistent runtime diagnostic state

--- SRCMainGS/Source/Main5.2/source/ModernGraphicsBootstrap.h
+++ SRCMainGS/Source/Main5.2/source/ModernGraphicsBootstrap.h
@@ -119,6 +119,7 @@ private:
     bool m_gl46Capable;
     bool m_active;
     bool m_initializationAttempted;
+    bool m_debugCallbackEnabled;
     ModernGraphicsBackend m_backend;
     ModernGraphicsOwnership m_ownership;
 
